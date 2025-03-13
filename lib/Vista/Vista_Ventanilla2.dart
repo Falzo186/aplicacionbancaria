f8d3af9a29b84edb37d3bb95be716fd11f289d89
LeslyDArts <a2221330001@alumnos.uat.edu.mx>
2025-03-13 04:58:01 -0600

    diseño ventanilla 2

--- lib/Vista/Vista_Ventanilla2.dart
+++ lib/Vista/Vista_Ventanilla2.dart
@@ -44,7 +44,9 @@ class _VistaVentanillaState extends State<VistaVentanilla2> {
         cuentaCliente!.saldo += monto;
       });
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Depósito de \$${monto.toStringAsFixed(2)} realizado.')),
+        SnackBar(
+          content: Text('Depósito de \$${monto.toStringAsFixed(2)} realizado.'),
+        ),
       );
     }
   }
@@ -60,12 +62,18 @@ class _VistaVentanillaState extends State<VistaVentanilla2> {
     double monto = double.tryParse(_montoController.text) ?? 0.0;
     if (monto < pagoMinimo) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('El pago mínimo es de \$${pagoMinimo.toStringAsFixed(2)}.')),
+        SnackBar(
+          content: Text(
+            'El pago mínimo es de \$${pagoMinimo.toStringAsFixed(2)}.',
+          ),
+        ),
       );
       return;
     }
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Pago de \$${monto.toStringAsFixed(2)} realizado.')),
+      SnackBar(
+        content: Text('Pago de \$${monto.toStringAsFixed(2)} realizado.'),
+      ),
     );
   }
 
@@ -83,7 +91,7 @@ class _VistaVentanillaState extends State<VistaVentanilla2> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Ventanilla - ${widget.cliente.nombreCompleto}'),
-        backgroundColor: const Color.fromARGB(255, 124, 85, 34),
+        backgroundColor: const Color(0xFF472F2F),
       ),
       body: Container(
         color: const Color.fromARGB(255, 185, 166, 141),
@@ -91,49 +99,90 @@ class _VistaVentanillaState extends State<VistaVentanilla2> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Cliente: ${widget.cliente.nombreCompleto}',
-                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            Text('Número de Cuenta: ${widget.cliente.numeroCuenta}',
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text('Teléfono: ${widget.cliente.telefono}',
-                style: const TextStyle(fontSize: 16)),
-            Text('Correo: ${widget.cliente.correoElectronico}',
-                style: const TextStyle(fontSize: 16)),
-            Text('Dirección: ${widget.cliente.direccionCompleta}',
-                style: const TextStyle(fontSize: 16)),
-            Text('Fecha de Nacimiento: $formattedDate',
-                style: const TextStyle(fontSize: 16)),
-            Text('Género: ${widget.cliente.genero}',
-                style: const TextStyle(fontSize: 16)),
-            Text('Tipo de Cuenta: ${cuentaCliente?.tipoCuenta ?? 'N/A'}',
-                style: const TextStyle(fontSize: 16)),
-            Text('Saldo: \$${cuentaCliente?.saldo.toStringAsFixed(2) ?? 'N/A'}',
-                style: const TextStyle(fontSize: 16)),
+            Text(
+              'Cliente: ${widget.cliente.nombreCompleto}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Número de Cuenta: ${widget.cliente.numeroCuenta}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Teléfono: ${widget.cliente.telefono}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Correo: ${widget.cliente.correoElectronico}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Dirección: ${widget.cliente.direccionCompleta}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Fecha de Nacimiento: $formattedDate',
+              style: const TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Género: ${widget.cliente.genero}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Tipo de Cuenta: ${cuentaCliente?.tipoCuenta ?? 'N/A'}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Saldo: \$${cuentaCliente?.saldo.toStringAsFixed(2) ?? 'N/A'}',
+              style: const TextStyle(fontSize: 16),
+            ),
             const SizedBox(height: 20),
             if (cuentaCliente != null)
-              Text('Número de Cuenta: ${cuentaCliente!.numeroCuenta}',
-                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text(
+                'Número de Cuenta: ${cuentaCliente!.numeroCuenta}',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             if (cuentaCredito != null) ...[
-              Text('Límite de Crédito: \$${cuentaCredito!.limiteCredito.toStringAsFixed(2)}',
-                  style: const TextStyle(fontSize: 16)),
-              Text('Crédito Disponible: \$${cuentaCredito!.limiteCredito - cuentaCredito!.saldoDeuda}',
-                  style: const TextStyle(fontSize: 16)),
-              Text('Saldo Deuda: \$${cuentaCredito!.saldoDeuda.toStringAsFixed(2)}',
-                  style: const TextStyle(fontSize: 16, color: Colors.red)),
-              Text('Tasa de Interés: ${cuentaCredito!.tasaInteres}%',
-                  style: const TextStyle(fontSize: 16,)),
+              Text(
+                'Límite de Crédito: \$${cuentaCredito!.limiteCredito.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Crédito Disponible: \$${cuentaCredito!.limiteCredito - cuentaCredito!.saldoDeuda}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Saldo Deuda: \$${cuentaCredito!.saldoDeuda.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16, color: Colors.red),
+              ),
+              Text(
+                'Tasa de Interés: ${cuentaCredito!.tasaInteres}%',
+                style: const TextStyle(fontSize: 16),
+              ),
             ],
             if (prestamo != null) ...[
-              Text('Deuda Pendiente: \$${prestamo!.monto.toStringAsFixed(2)}',
-                  style: const TextStyle(fontSize: 16, color: Colors.red)),
-              Text('Meses Restantes: ${prestamo!.meses - prestamo!.pagosRealizados}',
-                  style: const TextStyle(fontSize: 16,)),
-              Text('Tasa de Interés a meses: ${prestamo!.tasaInteres}%',
-                  style: const TextStyle(fontSize: 16,)),
-              Text('Pago Mínimo: \$${prestamo!.pagoMinimo?.toStringAsFixed(2)}',
-                  style: const TextStyle(fontSize: 16,)),
-              Text('Fecha de Pago: ${_formatDate(prestamo!.fechapago)}',
-                  style: const TextStyle(fontSize: 16,)),
+              Text(
+                'Deuda Pendiente: \$${prestamo!.monto.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16, color: Colors.red),
+              ),
+              Text(
+                'Meses Restantes: ${prestamo!.meses - prestamo!.pagosRealizados}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Tasa de Interés a meses: ${prestamo!.tasaInteres}%',
+                style: const TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Pago Mínimo: \$${prestamo!.pagoMinimo?.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Fecha de Pago: ${_formatDate(prestamo!.fechapago)}',
+                style: const TextStyle(fontSize: 16),
+              ),
             ],
             TextField(
               controller: _montoController,
